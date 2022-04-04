b2aaa0586c855933e5d5a34b2004587b88d7b4fb
sinat_18811413 <1833183060@qq.com>
2022-04-04 14:55:12 +0800

    添加成员函数访问操作符重载

---
+++ operator-overload-class-member-access.cpp
@@ -0,0 +1,57 @@
+/***************************************************************** 
+* Copyright (C) 2017 鸟哥  All rights reserved
+* filename: 
+* description：重载 运算符 -> (类成员访问).
+* author: 鸟哥
+* see：https://www.tutorialspoint.com/cplusplus/class_member_access_operator_overloading.htm
+* see:https://en.cppreference.com/w/cpp/language/operators
+*********************************************************************/
+
+#include <iostream>
+#include <string>
+using namespace std;
+class Tree{
+public:
+    Tree(){
+    }
+    void foo(){
+        cout<<"Tree::foo()"<<endl;
+    }
+private:    
+};
+//1、简单的例子
+class Handle{
+public:
+    Handle(Tree* t):tree(t){
+
+    }
+    Tree* operator->(){
+        return tree;
+    }
+private:
+    Tree* tree;
+};
+//2、使用类模板的例子
+template<typename T>
+class HandleT{
+public:
+    HandleT(T* o):obj(o){
+    }
+    T* operator->(){
+        return obj;
+    }
+private:
+    T* obj;
+};
+
+int main(){
+    Tree t;    
+    Handle handle{&t};
+    HandleT<Tree> handleT{&t};
+    handle->foo();
+    handleT->foo();
+    cout<<"------------------------------"<<endl;
+    
+    cout<<"enter any key to exit";
+    cin.get();
+}
--- operator-overload-subscribe.cpp
+++ operator-overload-subscribe.cpp
@@ -1,9 +1,10 @@
 /***************************************************************** 
 * Copyright (C) 2017 鸟哥  All rights reserved
 * filename: 
-* description：重载 运算符 [](下标运算符)
+* description：重载 运算符 [](下标运算符).
 * author: 鸟哥
 * see：https://www.tutorialspoint.com/cplusplus/subscripting_operator_overloading.htm
+* see:https://en.cppreference.com/w/cpp/language/operators
 *********************************************************************/
 
 #include <iostream>
@@ -14,20 +15,18 @@ class Tree{
 public:
     Tree(){
     }
-    double operator[](const string& key){        
+    double operator[](const string& key){//下标类型可以不是整数
         return atof(key.c_str());
     }
     int operator[](int i){
         return i*i;
     }
-    string operator[](const Water* w){
+    string operator[](const Water* w){//下标类型可以不是整数
         return "water";
     }
 private:
     
 };
-
-
 
 int main(){
     Tree t;    
